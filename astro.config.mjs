97256d843fab45ceb0b6704a6d6ac954f49c6fdc
Tomoya Eguchi <mail@cumulo.works>
2024-09-22 05:35:55 +0900

    format: "bracketSameLine": true

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
