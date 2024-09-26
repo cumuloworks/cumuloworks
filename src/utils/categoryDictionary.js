8c89ce75668d8ea285013cb89eb8c1cc77251850
Tomoya Eguchi <mail@cumulo.works>
2024-09-26 09:26:18 +0900

    refactor: categoryの説明をdictionaryに統合

---
+++ src/utils/categoryDictionary.js
@@ -0,0 +1,31 @@
+export const categoryDictionary = {
+	ads: {
+		name: "Ads / Corporate",
+		description: "Advertisements and promotional videos for businesses.",
+	},
+	entertainment: {
+		name: "Entertainment",
+		description:
+			"Music videos, live productions, and other entertainment content.",
+	},
+	techtips: {
+		name: "Technology / Tips",
+		description: "Technology and tips for CG artists.",
+	},
+	announcement: {
+		name: "Announcement",
+		description: "Announcements and other important information.",
+	},
+	aviutl: {
+		name: "Aviutl",
+		description: "Aviutl related content.",
+	},
+	ae: {
+		name: "After Effects",
+		description: "After Effects related content.",
+	},
+	others: {
+		name: "Others",
+		description: "Other content.",
+	},
+};
--- src/utils/categoryNameDictionary.js
+++
@@ -1,9 +0,0 @@
-export const categoryDictionary = {
-	ads: "Ads / Corporate",
-	entertainment: "Entertainment",
-	techtips: "Technology / Tips",
-	announcement: "Announcement",
-	aviutl: "Aviutl",
-	ae: "After Effects",
-	others: "Others",
-};
