39e82bfd0400b56ec3fbd7ac37d416a7c30281c7
Tomoya Eguchi <mail@cumulo.works>
2025-01-08 15:11:30 +0900

    change: フォント変更

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,8 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			sans: ["Inter", `"M PLUS 1p"`, "sans-serif"],
-			mono: ["monospace"],
+			sans: ["Geist", "Noto Sans JP", "sans-serif"],
+			mono: ["Geist Mono", "monospace"],
 		},
 		extend: {
 			brightness: {
